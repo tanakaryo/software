93d0473d277c207e02007019ee2f8e01f3f6dc87
tanakaryo <yoichiikegawa@YoichinoMacBook-Pro.local>
2024-01-20 00:28:57 +0900

    first feature.

---
+++ java/feature1/parse-xml/parse-xml/src/main/java/java-private-feature/App.java
@@ -0,0 +1,13 @@
+package java-private-feature;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
